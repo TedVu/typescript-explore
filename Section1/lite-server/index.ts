a8e9f350c666678897eb62d6247fabf8b291f8c6
TedEngineer <tedvu184@gmail.com>
2023-03-15 19:35:30 +1100

    Experiment enum type

--- Section1/lite-server/index.ts
+++ Section1/lite-server/index.ts
@@ -1,12 +1,18 @@
+enum Gender {
+  MALE,
+  FEMALE,
+}
 // object type
 const person: {
   name?: string;
   age?: number;
   hobbies?: string[];
   role?: [number, string];
+  gender?: Gender;
 } = {
   name: "Ted Vu",
   age: 30,
   hobbies: ["sport"],
   role: [5, "test"],
+  gender: Gender.FEMALE,
 };
