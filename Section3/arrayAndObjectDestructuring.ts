4d15a820af4b08d1cd9c393886a2dc3cfbeb96dc
TedEngineer <tedvu184@gmail.com>
2023-03-19 11:55:32 +1100

    Cleanup code example

--- Section3/arrayAndObjectDestructuring.ts
+++ Section3/arrayAndObjectDestructuring.ts
@@ -1,4 +1,4 @@
-// Array destructuring
+// Array destructuring - values are pulled out by order
 const hobbies2 = ["Reading", "Cooking"];
 
 const [hobby1, hobby2] = hobbies2;
@@ -9,7 +9,7 @@ const person4 = {
   fullName: "Ted Vu",
 };
 
-// Object destructuring
+// Object destructuring - values are pulled out based on keys
 const { fullName } = person4;
 
 console.log(`The full name is ${fullName}`);
