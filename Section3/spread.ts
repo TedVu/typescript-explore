ec04c937452f28205dc7c22326632fe1abaffdfd
TedEngineer <tedvu184@gmail.com>
2023-03-19 11:35:54 +1100

    Add example of using spread operator in object

--- Section3/spread.ts
+++ Section3/spread.ts
@@ -8,3 +8,12 @@ activeHobbies.push(...hobbies);
 activeHobbies.forEach((activeHobby) => {
   console.log(`Active hobby is ${activeHobby}`);
 });
+
+const person1 = {
+  firstName: "Ted",
+  lastName: "Vu",
+};
+
+const person3 = { ...person1 };
+
+console.log(`Person 3 first name is ${person3.firstName}`);
