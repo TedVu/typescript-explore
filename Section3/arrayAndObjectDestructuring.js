5ff276a8833ef203f89df326de4b5712be9842da
TedEngineer <tedvu184@gmail.com>
2023-03-19 11:53:45 +1100

    Add array and object destructuring examples

---
+++ Section3/arrayAndObjectDestructuring.js
@@ -0,0 +1,9 @@
+// Array destructuring
+var hobbies2 = ["Reading", "Cooking"];
+var hobby1 = hobbies2[0], hobby2 = hobbies2[1];
+console.log("Hobby 1 is ".concat(hobby1));
+var person4 = {
+    fullName: "Ted Vu"
+};
+var fullName = person4.fullName;
+console.log("The full name is ".concat(fullName));
---
+++ Section3/arrayAndObjectDestructuring.ts
@@ -0,0 +1,15 @@
+// Array destructuring
+const hobbies2 = ["Reading", "Cooking"];
+
+const [hobby1, hobby2] = hobbies2;
+
+console.log(`Hobby 1 is ${hobby1}`);
+
+const person4 = {
+  fullName: "Ted Vu",
+};
+
+// Object destructuring
+const { fullName } = person4;
+
+console.log(`The full name is ${fullName}`);
