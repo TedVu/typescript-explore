c8369a713930277c9c79415e906a439137365d2b
TedEngineer <tedvu184@gmail.com>
2023-03-23 20:36:27 +1100

    add example code for optional chaining

--- AdvancedTypings/index-property.ts
+++ AdvancedTypings/index-property.ts
@@ -1,7 +1,7 @@
 // index property says that we can add as many property we want to this interface and the property type is number and the value type is boolean
-interface ErrorContainer {
+type ErrorContainer = {
   [prop: number]: boolean;
-}
+};
 
 const errorBag: ErrorContainer = {
   1: true,
---
+++ AdvancedTypings/optional-chaining.ts
@@ -0,0 +1,9 @@
+const person = {
+  id: "1",
+  name: "Max",
+  job: {
+    id: "2",
+  },
+};
+
+console.log(person?.job?.id);
