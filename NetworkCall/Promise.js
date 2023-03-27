d3f4abf98e420d3d66c4c8a5f2e87ea9bfafdfea
TedEngineer <tedvu184@gmail.com>
2023-03-27 12:23:46 +1100

    Add example of resolving and rejecting promises

--- NetworkCall/Promise.js
+++ NetworkCall/Promise.js
@@ -3,3 +3,18 @@ const promise1 = Promise.resolve(123);
 promise1.then((value) => {
   console.log(`Value is ${value}`);
 });
+
+let p = new Promise((resolve, reject) => {
+  let a = 1 + 1;
+  if (a == 3) {
+    resolve("Success");
+  } else {
+    reject("Failed");
+  }
+});
+
+p.then((message) => {
+  console.log(`This is the message from calling resolve ${message}`);
+}).catch((message) => {
+  console.log(`This is the message from calling reject ${message}`);
+});
