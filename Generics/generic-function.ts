85b1b0ec367c3ddabe9e2008f8d2827d7dfee957
TedEngineer <tedvu184@gmail.com>
2023-04-03 16:06:14 +1000

    Add example when length exists on both type, and typescript allows

--- Generics/generic-function.ts
+++ Generics/generic-function.ts
@@ -6,3 +6,21 @@ function merge<T extends object, U>(objA: T, objB: U) {
 const objC = merge({ name: "ted" }, { age: 4 });
 
 console.log(objC.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+
+  if (element.length === 1) {
+    console.log("Got 1 element");
+  } else if (element.length > 1) {
+    console.log("Got " + element.length + " elements");
+  }
+
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Ted Vu"));
