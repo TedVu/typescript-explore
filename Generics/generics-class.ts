9b84e7c17a26811922f6ea6128882fc5e1dc5a49
Ted Vu <ted.vu@wisetechglobal.com>
2023-04-04 11:39:33 +1000

    Add example of using generics in class

---
+++ Generics/generics-class.ts
@@ -0,0 +1,18 @@
+class CustomStorage<T> {
+    private data : T[] = [];
+    
+    addItem(item : T) {
+        this.data.push(item);
+    }
+
+    removeItem(item : T){
+        this.data.splice(this.data.indexOf(item),1);
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const aStorage = new CustomStorage<string>();
+aStorage.addItem('ted');
