e799fda7d9d086adf0ab8ccc9ec141b021a6600a
TedEngineer <tedvu184@gmail.com>
2023-04-07 18:29:18 +1000

    Add code to handle edge case of remove item

--- Generics/generics-class.ts
+++ Generics/generics-class.ts
@@ -1,18 +1,22 @@
 class CustomStorage<T> {
-    private data : T[] = [];
-    
-    addItem(item : T) {
-        this.data.push(item);
-    }
+  private data: T[] = [];
 
-    removeItem(item : T){
-        this.data.splice(this.data.indexOf(item),1);
-    }
+  addItem(item: T) {
+    this.data.push(item);
+  }
 
-    getItems() {
-        return [...this.data];
+  removeItem(item: T) {
+    // this will result in an error if we cannot find item
+    if (this.data.indexOf(item) === -1) {
+      return;
     }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
 }
 
 const aStorage = new CustomStorage<string>();
-aStorage.addItem('ted');
+aStorage.addItem("ted");
