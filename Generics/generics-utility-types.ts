eaf750f7511809506e3ce20f99a7b3eb5c3680af
TedEngineer <tedvu184@gmail.com>
2023-04-07 18:45:03 +1000

    Add example of course goal using partial utility types

---
+++ Generics/generics-utility-types.ts
@@ -0,0 +1,19 @@
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+
+  courseGoal.title = "ted";
+  courseGoal.description = "kevin";
+  courseGoal.completeUntil = new Date();
+
+  return courseGoal as CourseGoal;
+}
